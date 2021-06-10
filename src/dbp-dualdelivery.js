cb8218d0c1466b6dd31bd24f47aab95e1abbd0b7
Patrizio Bekerle <patrizio@bekerle.com>
2021-06-10 12:52:51 +0200

    Integrate TU Graz specifics in AppShell slots

--- src/dbp-dualdelivery.js
+++ src/dbp-dualdelivery.js
@@ -1,4 +1,8 @@
 import {AppShell} from '@dbp-toolkit/app-shell';
 import * as commonUtils from '@dbp-toolkit/common/utils';
+import {Translated} from "@dbp-toolkit/common/src/translated";
+import {TUGrazLogo} from "@dbp-toolkit/app-shell/src/tugraz-logo";
 
 commonUtils.defineCustomElement('dbp-dualdelivery', AppShell);
+commonUtils.defineCustomElement('dbp-translated', Translated);
+commonUtils.defineCustomElement('dbp-tugraz-logo', TUGrazLogo);
