d287d7501631528e844b0ffe62ce6d781b72f2b9
Christoph Reiter <reiter.christoph@gmail.com>
2022-04-13 15:13:38 +0200

    Port to i18next-parser

---
+++ i18next-parser.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    createOldCatalogs: false,
+    indentation: 4,
+    keepRemoved: false,
+    locales: ['en', 'de'],
+    output: 'src/i18n/$LOCALE/$NAMESPACE.json',
+    input: ['src/**/*.js'],
+    sort: true,
+    i18nextOptions: {compatibilityJSON: 'v3'},
+};
