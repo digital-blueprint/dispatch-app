715762daf221b0eacb47bfaa2eccdcd2c62981d1
Christoph Reiter <reiter.christoph@gmail.com>
2022-04-13 15:06:24 +0200

    Update toolkit

--- src/dbp-dualdelivery.js
+++ src/dbp-dualdelivery.js
@@ -1,3 +1,4 @@
+import '@webcomponents/scoped-custom-element-registry';
 import {AppShell} from '@dbp-toolkit/app-shell';
 import * as commonUtils from '@dbp-toolkit/common/utils';
 import {Translated} from "@dbp-toolkit/common/src/translated";
