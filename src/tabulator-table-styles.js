56685daef61a10f1c6809094a2696a8a9f3a70a8
lila99 <r.gherman@tugraz.at>
2024-08-21 16:14:02 +0200

    better tabulator header style

--- src/tabulator-table-styles.js
+++ src/tabulator-table-styles.js
@@ -4,6 +4,9 @@ export function getTabulatorStyles() {
     // language=css
     return css`
         /* Define the style when the column is not sorted */
+        .tabulator .tabulator-header {
+            font-weight: normal;
+        }
         .tabulator .tabulator-header .tabulator-col.tabulator-sortable[aria-sort="none"] .tabulator-col-content .tabulator-col-sorter .tabulator-arrow{
             border-top: none;
             border-bottom: 6px solid var(--dbp-muted);
