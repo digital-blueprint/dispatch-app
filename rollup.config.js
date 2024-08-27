0accb8534558e73f8a6ca70353a871dfbd8d2448
lila99 <r.gherman@tugraz.at>
2024-08-27 11:07:36 +0200

    npm ci

--- rollup.config.js
+++ rollup.config.js
@@ -270,10 +270,14 @@ Dependencies:
 `,
                 },
                 thirdParty: {
-                    allow: {
-                        test: '(MIT OR BSD-3-Clause OR Apache-2.0 OR LGPL-2.1-or-later)',
-                        failOnUnlicensed: true,
-                        failOnViolation: true,
+                    allow(dependency) {
+                        let licenses = [
+                            'LGPL-2.1-or-later', 'MIT', 'BSD-3-Clause', 'Apache-2.0', 'BSD',
+                        ];
+                        if (!licenses.includes(dependency.license)) {
+                            throw new Error(`Unknown license for ${dependency.name}: ${dependency.license}`);
+                        }
+                        return true;
                     },
                 },
             }),
