ad0d6a91f577cf13e4242cceae91bc2b0ed6855c
zsuffa david <zsuffa.david@gmail.com>
2024-10-14 09:15:27 +0200

    fix linter error

--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,5 @@
-import {css, unsafeCSS} from 'lit';
-import {getIconSVGURL} from '@dbp-toolkit/common';
+import {css} from 'lit';
+// import {getIconSVGURL} from '@dbp-toolkit/common';
 
 // export function getDispatchRequestTableStyles() {
 //     // language=css
