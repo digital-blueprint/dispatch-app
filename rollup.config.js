233a0f2797006aab6b6e471ff1ca9a7cb91f0e9b
Christoph Reiter <reiter.christoph@gmail.com>
2025-04-10 16:50:22 +0200

    rollup: disable loose mode for babel to work around errors
    otherwise it breaks things the scoped mixin code

--- rollup.config.js
+++ rollup.config.js
@@ -454,7 +454,7 @@ Dependencies:
                     [
                         '@babel/preset-env',
                         {
-                            loose: true,
+                            loose: false,
                             shippedProposals: true,
                             bugfixes: true,
                             modules: false,
