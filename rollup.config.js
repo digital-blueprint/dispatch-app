7fc9829490887b4d92f2f3e2858feb5fcd315bd3
Christoph Reiter <reiter.christoph@gmail.com>
2025-10-30 12:59:28 +0100

    build: optimize font copy

--- rollup.config.js
+++ rollup.config.js
@@ -340,8 +340,9 @@ Dependencies:
                         },
                         {src: 'assets/silent-check-sso.html', dest: 'dist'},
                         {
-                            src: await getPackagePath('@fontsource/nunito-sans', '*'),
-                            dest: 'dist/' + (await getDistPath(pkg.name, 'fonts/nunito-sans')),
+                            src: await getPackagePath('@fontsource/nunito-sans', '.'),
+                            dest: 'dist/' + (await getDistPath(pkg.name, 'fonts')),
+                            rename: 'nunito-sans',
                         },
                         {
                             src: await getPackagePath('@dbp-toolkit/common', 'src/spinner.js'),
@@ -417,8 +418,9 @@ Dependencies:
                         },
                         {src: customAssetsPath + 'silent-check-sso.html', dest: 'dist'},
                         {
-                            src: await getPackagePath('@tugraz/font-source-sans-pro', 'files/*'),
-                            dest: 'dist/' + (await getDistPath(pkg.name, 'fonts/source-sans-pro')),
+                            src: await getPackagePath('@tugraz/font-source-sans-pro', 'files'),
+                            dest: 'dist/' + (await getDistPath(pkg.name, 'fonts')),
+                            rename: 'source-sans-pro',
                         },
                         {
                             src: await getPackagePath('@tugraz/web-components', 'src/spinner.js'),
