e4b82baf211b586d655d5f6bc20b9ebfbacf309d
Patrizio Bekerle <patrizio@bekerle.com>
2026-05-07 14:33:38 +0200

    devenv: add sshuttle
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- devenv.nix
+++ devenv.nix
@@ -1,5 +1,14 @@
 {
+  pkgs,
+  ...
+}:
+{
   # More config is provided by input shared
+
+  packages = with pkgs; [
+    sshuttle # For connecting to the dev server for API requests to Vendo
+  ];
+
   enterShell = ''
     echo "🛠️ DBP Dispatch App Dev Shell"
   '';
